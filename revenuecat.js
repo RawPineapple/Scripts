b5f99ac82d60162710da57f508f12ab01058612f
RawPineapple <95847271+RawPineapple@users.noreply.github.com>
2023-09-24 19:26:30 +0800

    Update revenuecat.js

--- revenuecat.js
+++ revenuecat.js
@@ -3,7 +3,7 @@
 [rewrite_local]
 
 # ～ RevenueCat@ddgksf2013
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-echo-response https://raw.githubusercontent.com/RawPineapple/Scripts/main/revenuecat.js
+^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-response-body https://raw.githubusercontent.com/RawPineapple/Scripts/main/revenuecat.js
 
 [mitm]
 
@@ -11,7 +11,7 @@ hostname=api.revenuecat.com
 
 ***********************************/
 
-var body = JSON.parse(JSON.stringify($response.body))
+var body = JSON.parse($response.body)
 
 body['subscriber']['subscriptions']['grow_1y_128']['expires_date']="2099-10-01T09:22:47Z";
 body['subscriber']['entitlements']['grow.pro']['expires_date']="2099-10-01T09:22:47Z";
