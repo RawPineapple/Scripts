8735b50e5c5bd924ca4b39fe5e9e5f516001156a
RawPineapple <95847271+RawPineapple@users.noreply.github.com>
2023-09-24 19:50:40 +0800

    Update revenuecat.js

--- revenuecat.js
+++ revenuecat.js
@@ -3,7 +3,7 @@
 [rewrite_local]
 
 # ～ RevenueCat@ddgksf2013
-^https:\/\/api\.revenuecat\.com\/.+\/(receipts$|subscribers\/[^/]+$) url script-response-body https://raw.githubusercontent.com/RawPineapple/Scripts/main/revenuecat.js
+^https:\/\/api\.revenuecat\.com\/v1\/(subscribers|receipts) url script-response-body https://raw.githubusercontent.com/RawPineapple/Scripts/main/revenuecat.js
 
 [mitm]
 
@@ -11,9 +11,48 @@ hostname=api.revenuecat.com
 
 ***********************************/
 
-var body = JSON.parse($response.body);
+var obj = {
+  "request_date_ms" : 1695555312858,
+  "request_date" : "2023-09-24T11:35:12Z",
+  "subscriber" : {
+    "non_subscriptions" : {
 
-body.subscriber.subscriptions.grow_1y_128.expires_date = "2099-10-01T09:22:47Z";
-body.subscriber.entitlements.['grow.pro'].expires_date= "2099-10-01T09:22:47Z";
+    },
+    "first_seen" : "2023-09-24T08:51:38Z",
+    "original_application_version" : "250",
+    "other_purchases" : {
 
-$done({body:JSON.stringify(body)});
+    },
+    "management_url" : "https://apps.apple.com/account/subscriptions",
+    "subscriptions" : {
+      "grow_1y_128" : {
+        "original_purchase_date" : "2023-09-24T09:22:48Z",
+        "expires_date" : "2099-10-01T09:22:47Z",
+        "is_sandbox" : false,
+        "refunded_at" : null,
+        "store_transaction_id" : "380001353687731",
+        "unsubscribe_detected_at" : "2023-09-24T09:47:56Z",
+        "grace_period_expires_date" : null,
+        "period_type" : "trial",
+        "purchase_date" : "2023-09-24T09:22:47Z",
+        "billing_issues_detected_at" : null,
+        "ownership_type" : "PURCHASED",
+        "store" : "app_store",
+        "auto_resume_date" : null
+      }
+    },
+    "entitlements" : {
+      "grow.pro" : {
+        "grace_period_expires_date" : null,
+        "purchase_date" : "2023-09-24T09:22:47Z",
+        "product_identifier" : "grow_1y_128",
+        "expires_date" : "2099-10-01T09:22:47Z"
+      }
+    },
+    "original_purchase_date" : "2023-09-24T08:13:13Z",
+    "original_app_user_id" : "$RCAnonymousID:9d16cc33535c408a9cb2604d9080e736",
+    "last_seen" : "2023-09-24T08:51:38Z"
+  }
+}
+
+$done({body:JSON.stringify(obj)});
